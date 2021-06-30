84784a9ee38f76b0e0396a5d2d4025522f563d70
eric0716336 <ericsteven00@gmail.com>
2021-06-30 10:54:32 +0800

    Delete app directory

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class KeyForm(forms.Form):
-    pass1= forms.CharField(label="New password", max_length=10)
-    pass2= forms.CharField(label="Re-enter new password", max_length=10)
-
-class cardname(forms.Form):
-    holdername= forms.CharField(label="Card holder name", max_length=10)
